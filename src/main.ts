5bfb781469e107015071787f52984030fa1fcc2a
Dmitry Bondarenko <d.i.bondarenko@yandex.ru>
2020-05-24 15:52:04 +0300

    :sparkles: First try of new action.
    Simple test for debug output and adding label on assigned issue.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,45 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const token = core.getInput('repo-token', { required: true });
+    const context = github.context;
+    core.debug(`Context: ${context}`);
+    const client = new github.GitHub(token);
+    const repository = await client.graphql(
+      `
+       query issue($owner: String!, $name: String!, $issue: Int!) {
+          repository(owner: $owner, name: $repo) {
+            issue(number: $issue) {
+              projectCards {
+                nodes {
+                  project {
+                    name
+                  }
+                }
+              }
+              labels(first: 10) {
+                nodes {
+                  name
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue: context.issue.number
+      });
+    core.debug(`Response: ${repository}`);
+    await client.issues.addLabels({
+      issue_number: context.issue.number,
+      labels: ["good first issue"],
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    });
   } catch (error) {
     core.setFailed(error.message)
   }
